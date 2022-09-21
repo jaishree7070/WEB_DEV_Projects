ccf31ecab13fccf450c32f602b19b9ba859cc708
R Jaishree <109673030+jaishree7070@users.noreply.github.com>
2022-09-21 19:47:04 +0530

    Calculator
    Designed the Calculator with html,
    Using table-rows and table-columns and with DOM manipulation and eval() function.

---
+++ calculator/script.js
@@ -0,0 +1,12 @@
+function display(val) {
+    document.getElementById("textval").value += val;
+    return val
+}
+function result() {
+    let x = document.getElementById("textval").value;
+    let y = eval(x);
+    document.getElementById("textval").value = y;
+}
+function clr() {
+    document.getElementById("textval").value = "";
+}
