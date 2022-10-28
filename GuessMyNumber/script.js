5a235f12367aa078e8ae64a2d702abcf00c8debc
R Jaishree <109673030+jaishree7070@users.noreply.github.com>
2022-10-28 10:36:09 +0530

    First  commit

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -57,8 +57,6 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
-
-
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
